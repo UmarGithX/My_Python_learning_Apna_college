240ac8536d98fdf2e10bef30dc817b43559b18ef
Umar <12345678+UmarGithX@users.noreply.github.com>
2025-09-03 08:44:42 +0500

    changes Done

---
+++ lecture 7/file I-O.py
@@ -0,0 +1,11 @@
+# Python can be used to perform operations on a file. (read & write data)
+f = open("demo.txt" , "a+")
+data = f.write("\nthis is the best exaple i ever seen in my lisfe")
+print(data)
+print (type (data))
+f. close()
+
+import os
+with open("samole.txt","a+") as f:
+    f.write("what is the name of pakistan")
+    
